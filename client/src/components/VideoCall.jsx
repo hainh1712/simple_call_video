348e74492fd548716185da13db65e13f829f0bc8
hainh1712 <nguyenhoanghai20021712@gmail.com>
2023-12-19 14:27:47 +0700

    update  call vid

--- client/src/components/VideoCall.jsx
+++ client/src/components/VideoCall.jsx
@@ -1,3 +1,5 @@
+import Header from "./Header";
+import { useAuth } from "../contexts/AuthContext";
 import React, { useEffect, useRef, useState } from "react";
 import Peer from "simple-peer";
 import { io } from "socket.io-client";
@@ -5,6 +7,8 @@ import { io } from "socket.io-client";
 const socket = io("http://localhost:5000");
 
 function VideoCall() {
+  const authInfo = useAuth();
+  const { user } = authInfo;
   const [me, setMe] = useState("");
   const [stream, setStream] = useState(null);
   const [receivingCall, setReceivingCall] = useState(false);
@@ -97,94 +101,114 @@ function VideoCall() {
 
   const leaveCall = () => {
     setCallEnded(true);
-    if (connectionRef.current) {
-      connectionRef.current.destroy();
-    }
+    // if (connectionRef.current) {
+    //   connectionRef.current.destroy();
+    // }
   };
 
   return (
-    <div>
-      <div className="flex flex-row h-full w-full justify-center gap-[15%] z-">
-        <div>
-          <div className="flex-grow flex flex-col items-center justify-center h-[90%]">
-            <span className="text-red-500 font-bold text-3xl mb-4">Basic React JS video calling</span>
-            <span className="text-white font-bold text-md mb-4 text-center underline">
-              Copy your ID and anyone using the same server can use it to call you and vice versa!
-            </span>
-            <div className="flex flex-row gap-32">
-              <div className="flex flex-col items-center justify-center w-full">
-                <div className="video">
-                  {stream && (
-                    <video
-                      className="rounded-full"
-                      playsInline
-                      muted
-                      ref={myVideo}
-                      autoPlay
-                      style={{ width: "300px" }}
-                    />
-                  )}
-                </div>
-                <span className="text-white font-bold text-lg mb-4">{caller}</span>
-                <p className="text-white">{me}</p>
-              </div>
-
-              <div className="flex flex-col items-center justify-center w-full">
-                {callAccepted && !callEnded ? (
-                  <video
-                    className="rounded-full"
-                    playsInline
-                    ref={userVideo}
-                    autoPlay
-                    style={{ width: "300px" }}
-                  />
-                ) : (
-                  <div className="flex flex-col justify-center items-center">
-                    <img
-                      src="https://w0.peakpx.com/wallpaper/416/423/HD-wallpaper-devil-boy-in-mask-red-hoodie-dark-background-4ef517.jpg"
-                      className="rounded-full w-[15rem]"
-                      alt="User Avatar"
-                    />
-                    <span className="text-white font-bold text-lg">{idToCall}</span>
-                  </div>
-                )}
-              </div>
-            </div>
-            <textarea
-              className="text-black"
-              value={idToCall}
-              onChange={(e) => {
-                setIdToCall(e.target.value);
-              }}
-            />
-            <div>
-              {callAccepted && !callEnded ? (
-                <button className="text-black hover:text-gray-400 mr-6 font-bold bg-white rounded-md m-4 px-2" onClick={leaveCall}>
-                  End Call
-                </button>
-              ) : (
-                <button
-                  className="text-black hover:text-gray-400 mr-6 font-bold bg-white rounded-md m-4 px-2"
-                  onClick={() => callUser(idToCall)}
-                >
-                  Call
-                </button>
+    <div className="h-screen w-screen bg-gray-100">
+      <Header />
+      <div className="flex-grow flex flex-col items-center justify-center w-4/5 mx-auto">
+        <div className="flex flex-row gap-32">
+          <div className="flex flex-col items-center justify-center w-full">
+            <div className="video">
+              {stream && (
+                <video
+                  className="rounded-lg w-full h-full"
+                  playsInline
+                  muted
+                  ref={myVideo}
+                  autoPlay
+                  // style={{ width: "300px", height: "300px" }}
+                />
               )}
             </div>
-            <div className="text-white">
-              {receivingCall && !callAccepted ? (
-                <div className="caller flex flex-col">
-                  <h1 className="text-white">{caller} is calling...</h1>
-                  <button
-                    className="text-black text-xl hover:text-gray-400 mr-6 font-bold bg-white rounded-md m-4 px-2"
-                    onClick={answerCall}
-                  >
-                    Answer
-                  </button>
-                </div>
-              ) : null}
-            </div>
+            {/* <span className="text-red-500 font-bold text-lg mb-4">{caller}</span> */}
+            {/* <div className="info-person1 flex items-center justify-center my-2">
+              <div className="mr-2">
+                <img src={user?.avatar_url ? user.avatar_url : "/social-media.png"} alt="icon" width={36} height={36}></img>
+              </div>
+              <div>
+                <span>{user.name}</span><br/>
+                <span>Lớp: {user.classname} - {user.grade}</span>
+              </div> 
+            </div> */}
+            <p className="text-red-500">{me}</p>
           </div>
+
+          <div className="flex flex-col items-center justify-center w-full">
+            {callAccepted && !callEnded ? (
+              <>
+                <video
+                  className="rounded-lg w-full h-full"
+                  playsInline
+                  ref={userVideo}
+                  autoPlay
+                  // style={{ width: "300px", height: "300px" }}
+                />
+                {/* <div className="info-person2 flex items-center justify-center my-2">
+                  <div className="mr-2">
+                    <img src={user?.avatar_url ? user.avatar_url : "/social-media.png"} alt="icon" width={36} height={36}></img>
+                  </div>
+                  <div>
+                    <span>{user.name}</span><br/>
+                    <span>Lớp: {user.classname} - {user.grade}</span>
+                  </div> 
+                </div> */}
+                <span className="text-red-500 font-bold text-lg mb-4">{caller}</span>
+              </>        
+            ) : (
+              <div className="flex flex-col justify-center items-center">
+                <img
+                  src="https://w0.peakpx.com/wallpaper/416/423/HD-wallpaper-devil-boy-in-mask-red-hoodie-dark-background-4ef517.jpg"
+                  className="rounded-lg"
+                  alt="User Avatar"
+                  style={{ width: "400px", height: "400px" }}
+                />
+                {/* <div className="info-person2 flex items-center justify-center my-2">
+                  <div className="mr-2">
+                    <img src={user?.avatar_url ? user.avatar_url : "/social-media.png"} alt="icon" width={36} height={36}></img>
+                  </div>
+                  <span>{callingUser || 'Đang tìm kiếm'}</span>
+                </div> */}
+                <span className="text-red-500 font-bold text-lg">{idToCall}</span>
+              </div>
+            )}
+          </div>
+        </div>
+        <textarea 
+          className="text-black"
+          value={idToCall}
+          onChange={(e) => {
+            setIdToCall(e.target.value);}}
+        />
+        <div>
+          {callAccepted && !callEnded ? (
+            <button className="text-white hover:text-blue-100 font-bold rounded-md m-4 px-2 bg-[#D57658]" onClick={leaveCall}>
+              Stop
+            </button>
+          ) : (
+            <button
+              className="text-white hover:text-blue-100 font-bold rounded-md m-4 px-2 bg-[#50B58D]"
+              onClick={() => callUser(idToCall)}
+            >
+              Start
+            </button>
+          )}
+        </div>
+        <div className="text-red-500">
+          {receivingCall && !callAccepted ? (
+            <div className="caller flex flex-col">
+              <h1 className="text-red-500">{caller} is calling...</h1>
+              <button
+                className="text-black text-xl hover:text-gray-400 mr-6 font-bold bg-white rounded-md m-4 px-2"
+                onClick={answerCall}
+              >
+                Answer
+              </button>
+            </div>
+          ) : null}
         </div>
       </div>
     </div>
